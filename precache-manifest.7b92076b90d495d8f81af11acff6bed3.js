3b3ac8e6a0387722ca2ac18df9195f736f411702
Guillaume Munsch <guillaume@zap.lu>
2020-03-11 14:23:38 +0100

    game-of-life-test

---
+++ precache-manifest.7b92076b90d495d8f81af11acff6bed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51dc8663480ce069ac1a1f39329f9ef4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "749fef0371619250b9ab",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ef5cb4cfe60feeae240f",
+    "url": "/static/js/2.03710295.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.03710295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "749fef0371619250b9ab",
+    "url": "/static/js/main.17349f04.chunk.js"
+  },
+  {
+    "revision": "2420cbf4016a42d21877",
+    "url": "/static/js/runtime-main.2402ec6c.js"
+  }
+]);
