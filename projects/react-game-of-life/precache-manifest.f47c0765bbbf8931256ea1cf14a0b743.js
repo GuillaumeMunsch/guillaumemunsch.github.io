ccd5d7e720191405d285bc54614b25d478762d41
Guillaume Munsch <guigui.munsch@gmail.com>
2020-03-14 13:14:39 +0100

    Publishing game of life

---
+++ projects/react-game-of-life/precache-manifest.f47c0765bbbf8931256ea1cf14a0b743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8946b97756c3990d866be02e3d275024",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c4336aa7dab63d79ce3",
+    "url": "/static/css/main.58998a85.chunk.css"
+  },
+  {
+    "revision": "ef5cb4cfe60feeae240f",
+    "url": "/static/js/2.03710295.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.03710295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4336aa7dab63d79ce3",
+    "url": "/static/js/main.47bb2ce7.chunk.js"
+  },
+  {
+    "revision": "2420cbf4016a42d21877",
+    "url": "/static/js/runtime-main.2402ec6c.js"
+  }
+]);
